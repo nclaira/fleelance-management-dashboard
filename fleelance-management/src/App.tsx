665a1a58b307f3b27eba3ad645efc1036f41f149
nclaira <nclaire981@gmail.com>
2025-10-31 23:18:10 +0200

    create a new react + TypeScript app  and tailwind css

---
+++ fleelance-management/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="p-6 bg-gray-100 min-h-screen">
+      <h1 className="text-3xl font-bold text-green-600">Freelance Dashboard</h1>
+      <p className="text-gray-600">Tailwind is working perfectly ✅</p>
+    </div>
+  );
+}
+
+export default App;
